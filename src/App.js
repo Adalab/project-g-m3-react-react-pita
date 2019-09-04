7c3d5dfb7a2e8e434658c59653dff9b073a67266
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-09-04 21:11:29 +0200

    import landing from awesome cards project m2 and restore sass problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import logoAdalab from './images/logo-adalab-80px.png';
+import './scss/main.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <main className="page__main">
+          <div className="main__container">
+            <div className="main__wrapper">
+              <div className="main__title-container">
+                <h1 className="h1__title">Awesome profile cards</h1>
+              </div>
+              <div className="main__description">
+                <h2 className="description__title">Crea tu tarjeta de visita</h2>
+                <p className="description__text">Crea mejores contactos profesionales de forma fácil y cómoda</p>
+              </div>
+              <ul className="main__steps">
+                <li className="steps__item">
+                  <div className="item__container"> <i className="item__icon far fa-object-ungroup">
+                  </i>
+                    <span className="item__name">diseña</span>
+                  </div>
+                </li>
+                <li className="steps__item">
+                  <div className="item__container"> <i className="item__icon far fa-keyboard"></i>
+                    <span className="item__name">rellena</span>
+                  </div>
+                </li>
+                <li className="steps__item">
+                  <div className="item__container"> <i className="item__icon fas fa-share-alt"></i>
+                    <span className="item__name">comparte</span>
+                  </div>
+                </li>
+              </ul>
+            </div> <a className="start__link" href="cards.html">comenzar</a>
+          </div>
+        </main>
+        <footer className="page__footer"> <span className="footer__copyright">Awesome profile-cards @2019</span>
+          <a className="footer__link"
+            href="https://adalab.es/" target="blank">
+            <img className="footer__logo" src={logoAdalab} alt="Logotipo de Adalab" />
+          </a>
+        </footer>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
+
