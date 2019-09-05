3978fd9c9d199d3059a1934b112ac7f44c018d0c
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-09-05 16:43:57 +0200

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="preview-card js__preview-card">
+                <div className="preview-card__title-container">
+                    <h4 className="preview-card__name">Nombre Apellido</h4>
+                    <h5 className="preview-card__career">Front-end developer</h5>
+                </div>
+                <div className="preview-card__img js__profile-image"></div>
+                <ul className="preview-card__social-list">
+                    <li className="social-list__item hidden"> <a href="#phone" target="_blank" className="social-list__link phone__link">
+                        <i className="fas fa-mobile-alt social-list__icon"></i>
+                    </a>
+                    </li>
+                    <li className="social-list__item hidden"> <a href="#email" target="_blank" className="social-list__link email__link">
+                        <i className="far fa-envelope social-list__icon"></i>
+                    </a>
+                    </li>
+                    <li className="social-list__item hidden"> <a href="#linkedin" target="_blank" className="social-list__link linkedin__link">
+                        <i className="fab fa-linkedin-in social-list__icon"></i>
+                    </a>
+                    </li>
+                    <li className="social-list__item hidden"> <a href="#github" target="_blank" className="social-list__link github__link">
+                        <i className="fab fa-github-alt social-list__icon"></i>
+                    </a>
+
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Card;
--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import Reset from './Reset';
+import Card from './Card';
 
 class CardPreview extends React.Component {
     render() {
@@ -7,32 +8,7 @@ class CardPreview extends React.Component {
             <section className="card-preview">
             <div className="card-preview__container">
               <Reset />
-              <div className="preview-card js__preview-card">
-                <div className="preview-card__title-container">
-                  <h4 className="preview-card__name">Nombre Apellido</h4>
-                  <h5 className="preview-card__career">Front-end developer</h5>
-                </div>
-                <div className="preview-card__img js__profile-image"></div>
-                <ul className="preview-card__social-list">
-                  <li className="social-list__item hidden"> <a href="#phone" target="_blank" className="social-list__link phone__link">
-                    <i className="fas fa-mobile-alt social-list__icon"></i>
-                  </a>
-                  </li>
-                  <li className="social-list__item hidden"> <a href="#email" target="_blank" className="social-list__link email__link">
-                    <i className="far fa-envelope social-list__icon"></i>
-                  </a>
-                  </li>
-                  <li className="social-list__item hidden"> <a href="#linkedin" target="_blank" className="social-list__link linkedin__link">
-                    <i className="fab fa-linkedin-in social-list__icon"></i>
-                  </a>
-                  </li>
-                  <li className="social-list__item hidden"> <a href="#github" target="_blank" className="social-list__link github__link">
-                    <i className="fab fa-github-alt social-list__icon"></i>
-                  </a>
-
-                  </li>
-                </ul>
-              </div>
+              <Card />
             </div>
           </section>
         )
