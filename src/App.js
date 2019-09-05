29d19a6473bebfd2d4ecda99ee262050436c7f69
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-09-05 13:29:25 +0200

    add props to style pallete of cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './scss/main.scss';
-import LandingMain from './components/LandingMain';
+/*import LandingMain from './components/LandingMain';*/
 import Cards from './components/Cards';
+
+/*<LandingMain />*/
+const colorPallete1=['#104E43','#438792','#a2deaf'];
+const colorPallete2=['#420101','#bd1010','#e95626'];
+const colorPallete3 =['#3e5b65','#eab052','#a0c0cf'];
 
 class App extends Component {
   render() {
     return (
-    <LandingMain />
+    
+    <Cards color={colorPallete1} color2={colorPallete2} color3={colorPallete3}/>
     );
   }
 }
