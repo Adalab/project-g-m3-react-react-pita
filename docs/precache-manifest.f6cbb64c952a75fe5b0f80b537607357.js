a1d2de728ba856f8eb4a8db77428ff6fc2010f22
Alba Gómez Pérez <albagope@gmail.com>
2019-09-11 13:13:34 +0200

    add GitPages

---
+++ docs/precache-manifest.f6cbb64c952a75fe5b0f80b537607357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6224c05c44a7129720ecdf1adfaa31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb7ca8e85885fb29c588",
+    "url": "./static/css/main.9a87ddef.chunk.css"
+  },
+  {
+    "revision": "69503c5c8a1d3b932aa7",
+    "url": "./static/js/2.18752cca.chunk.js"
+  },
+  {
+    "revision": "fb7ca8e85885fb29c588",
+    "url": "./static/js/main.3977c57d.chunk.js"
+  },
+  {
+    "revision": "368725d2af3826e97860",
+    "url": "./static/js/runtime~main.6d61fdb0.js"
+  },
+  {
+    "revision": "f5d4be6ddd2d27eb9413e9e417375077",
+    "url": "./static/media/tarjetas-molonas.f5d4be6d.png"
+  }
+]);
