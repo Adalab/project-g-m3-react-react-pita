640d56d7f3adb02ef1c8e294dcf5ecf8e07202fb
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-16 11:16:56 +0200

    add prop types from Cards

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
     super(props);
     this.state = {
 		formObject : {
-      		palette : 1, 
-      		FullName : '',
+      palette : 1, 
+      FullName : '',
 			job : '',
 			phone : '',
 			mail : '',
